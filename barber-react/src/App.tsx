8a537979c94dbb2f3c935bee863b79265ebde3f3
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-13 11:55:22 -0300

    feat: add Prettier and Tailwind CSS plugin, update Text component with color variants, and create App layout

--- barber-react/src/App.tsx
+++ barber-react/src/App.tsx
@@ -1,16 +1,23 @@
-
 import { Text } from "./components/text";
 
 export const App = () => {
-  return (
-    <div className="flex flex-col gap-2 items-center py-6 bg-gray-800">
-      <div className="flex gap-2">
-        <Text variant={"body-sm"}>Teste</Text>
-        <Text>Teste</Text>
-        <Text variant={"body-md-bold"}>Teste</Text>
-        <Text variant={"body-title-md"}>Teste</Text>
-        <Text variant={"body-title-lg"}>Teste</Text>
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center gap-2 py-6">
+            <div className="flex gap-2">
+                <Text color={"default"} variant={"body-sm"}>
+                    Teste
+                </Text>
+                <Text color={"muted"}>Teste</Text>
+                <Text color={"medium"} variant={"body-md-bold"}>
+                    Teste
+                </Text>
+                <Text color={"medium"} variant={"body-title-md"}>
+                    Teste
+                </Text>
+                <Text color={"high"} variant={"body-title-lg"}>
+                    Teste
+                </Text>
+            </div>
+        </div>
+    );
 };
--- barber-react/src/components/text.tsx
+++ barber-react/src/components/text.tsx
@@ -3,49 +3,49 @@ import type { VariantProps } from "class-variance-authority";
 import { cva } from "class-variance-authority";
 
 export const textVariants = cva("font-sans ", {
-  variants: {
-    variant: {
-      "body-sm": "text-sm leading-5 font-normal",
-      "body-md": "text-base leading-6 font-normal",
-      "body-md-bold": "text-base leading-6 font-semibold",
+    variants: {
+        variant: {
+            "body-sm": "text-sm leading-5 font-normal",
+            "body-md": "text-base leading-6 font-normal",
+            "body-md-bold": "text-base leading-6 font-semibold",
 
-      "body-title-md": "text-md leading-6 font-bold",
-      "body-title-lg": "text-lg leading-6 font-bold",
+            "body-title-md": "text-md leading-6 font-bold",
+            "body-title-lg": "text-lg leading-6 font-bold",
+        },
+        color: {
+            high: "text-gray-100",
+            medium: "text-gray-200",
+            default: "text-gray-300",
+            muted: "text-gray-500",
+            highlight: "text-yellow",
+        },
     },
-    color: {
-      default: "text-gray-300",
-      muted: "text-gray-500",
-      primary: "text-gray-100",
-      secondary: "text-gray-200",
-      highlight: "text-yellow",
+    defaultVariants: {
+        variant: "body-md",
+        color: "default",
     },
-  },
-  defaultVariants: {
-    variant: "body-md",
-    color: "default",
-  },
 });
 
 interface TextProps extends VariantProps<typeof textVariants> {
-  as?: keyof React.JSX.IntrinsicElements;
-  className?: string;
-  children?: React.ReactNode;
+    as?: keyof React.JSX.IntrinsicElements;
+    className?: string;
+    children?: React.ReactNode;
 }
 
 export const Text = ({
-  as = "span",
-  variant,
-  color,
-  className,
-  children,
-  ...props
-}: TextProps) => {
-  return React.createElement(
-    as,
-    {
-      className: textVariants({ variant, color, className }),
-      ...props,
-    },
+    as = "span",
+    variant,
+    color,
+    className,
     children,
-  );
+    ...props
+}: TextProps) => {
+    return React.createElement(
+        as,
+        {
+            className: textVariants({ variant, color, className }),
+            ...props,
+        },
+        children,
+    );
 };
