98a62896d9d0c9e6513553bd71da829235b9341e
Eduardo Da Silva Paim <166542880+Edu-2de@users.noreply.github.com>
2026-03-13 11:35:02 -0300

    feat: add Text component with variant support and update App component to utilize it

--- barber-react/src/App.tsx
+++ barber-react/src/App.tsx
@@ -1,9 +1,16 @@
-import "./App.css";
+
+import { Text } from "./components/text";
 
 export const App = () => {
   return (
-    <>
-      
-    </>
+    <div className="flex flex-col gap-2 items-center py-6 bg-gray-800">
+      <div className="flex gap-2">
+        <Text variant={"body-sm"}>Teste</Text>
+        <Text>Teste</Text>
+        <Text variant={"body-md-bold"}>Teste</Text>
+        <Text variant={"body-title-md"}>Teste</Text>
+        <Text variant={"body-title-lg"}>Teste</Text>
+      </div>
+    </div>
   );
 };
---
+++ barber-react/src/components/text.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
+
+export const textVariants = cva("font-sans ", {
+  variants: {
+    variant: {
+      "body-sm": "text-sm leading-5 font-normal",
+      "body-md": "text-base leading-6 font-normal",
+      "body-md-bold": "text-base leading-6 font-semibold",
+
+      "body-title-md": "text-md leading-6 font-bold",
+      "body-title-lg": "text-lg leading-6 font-bold",
+    },
+    color: {
+      default: "text-gray-300",
+      muted: "text-gray-500",
+      primary: "text-gray-100",
+      secondary: "text-gray-200",
+      highlight: "text-yellow",
+    },
+  },
+  defaultVariants: {
+    variant: "body-md",
+    color: "default",
+  },
+});
+
+interface TextProps extends VariantProps<typeof textVariants> {
+  as?: keyof React.JSX.IntrinsicElements;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+export const Text = ({
+  as = "span",
+  variant,
+  color,
+  className,
+  children,
+  ...props
+}: TextProps) => {
+  return React.createElement(
+    as,
+    {
+      className: textVariants({ variant, color, className }),
+      ...props,
+    },
+    children,
+  );
+};
